0ba6f6a7d84ff7e1dbb2841080562e2e2a09edce
Abishake007 <abishakearunagirinathan16@gmail.com>
2025-08-13 15:26:24 +0530

    github

---
+++ backend/src/main/java/com/freshnest/entity/User.java
@@ -0,0 +1,62 @@
+package com.freshnest.entity;
+
+import jakarta.persistence.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Builder.ObtainVia;
+
+@Data
+@Entity
+
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+   
+
+    @Lob
+    @Column(columnDefinition = "LongBlob")
+    private byte[] img;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
